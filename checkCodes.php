fd50ee589bdb287feea05f1dca900ce79cc788e7
Sebastian Hresko <sebastian.hresko@student.tuke.sk>
2024-08-16 23:46:14 +0200

    vsg

--- checkCodes.php
+++ checkCodes.php
@@ -5,7 +5,7 @@
   $conn = pg_connect("host=postgresql.r5.websupport.sk port=5432 dbname=gengi_web_db user=gengi password=Roland2022");
   if($conn) {
     $result = pg_query($conn, "SELECT * FROM codes where code = '".$data["code"]."'");
-    
+    echo $data["code"];
     if($result == false){
       echo 'somethings wrong brou';
     }
