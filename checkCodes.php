7aecfd7435149c74e43141651fc0b30aa1b8bf25
Sebastian Hresko <sebastian.hresko@student.tuke.sk>
2024-08-16 20:02:02 +0200

    viac saturovane triko

--- checkCodes.php
+++ checkCodes.php
@@ -1,26 +1,23 @@
 <?php
-// Allow requests from any origin
-  header("Access-Control-Allow-Origin: *");
-  // Allow the content type header
-  header("Access-Control-Allow-Headers: Content-Type");
   $jsonData = file_get_contents('php://input');
   $data = json_decode($jsonData, true);
 
   $conn = pg_connect("host=postgresql.r5.websupport.sk port=5432 dbname=gengi_web_db user=gengi password=Roland2022");
   if($conn) {
-    $result = pg_query($conn, "SELECT expired FROM codes where code = '".$data["code"]."'");
+    $result = pg_query($conn, "SELECT * FROM codes where code = '".$data["code"]."'");
+    
     if($result == false){
-      echo 'Somethings wrong with code check brou';
+      echo 'somethings wrong brou';
     }
     else{
       if(pg_num_rows($result) == 0){
-        echo 'Seems like wrong code brou';
+        echo 'wrong koud brout';
       }
       else{
-        echo pg_fetch_result($result, 0, 0);
+        echo 'lesgo';
       }
     }
  } else {
-     echo 'Connection to DB failed brou';
+     echo 'somethings wrong brou';
  }
 ?>
