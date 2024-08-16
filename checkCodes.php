1e544331a77327d45d92f75e37bb8f38ba88db24
Sebastian Hresko <sebastian.hresko@student.tuke.sk>
2024-08-16 23:50:04 +0200

    asi fixed kody

--- checkCodes.php
+++ checkCodes.php
@@ -1,23 +1,26 @@
 <?php
+// Allow requests from any origin
+  header("Access-Control-Allow-Origin: *");
+  // Allow the content type header
+  header("Access-Control-Allow-Headers: Content-Type");
   $jsonData = file_get_contents('php://input');
   $data = json_decode($jsonData, true);
 
   $conn = pg_connect("host=postgresql.r5.websupport.sk port=5432 dbname=gengi_web_db user=gengi password=Roland2022");
   if($conn) {
-    $result = pg_query($conn, "SELECT * FROM codes where code = '".$data["code"]."'");
-    echo $data["code"];
+    $result = pg_query($conn, "SELECT expired FROM codes where code = '".$data["code"]."'");
     if($result == false){
-      echo 'somethings wrong brou';
+      echo 'Somethings wrong with code check brou';
     }
     else{
       if(pg_num_rows($result) == 0){
-        echo 'wrong koud brout';
+        echo 'Seems like wrong code brou';
       }
       else{
-        echo 'lesgo';
+        echo pg_fetch_result($result, 0, 0);
       }
     }
  } else {
-     echo 'somethings wrong brou';
+     echo 'Connection to DB failed brou';
  }
 ?>
